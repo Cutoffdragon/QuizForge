218b60acf65d1af9b9dcdf0a336999f6bcc8e668
Cutoffdragon <jasonlagrasse@gmail.com>
2024-07-16 19:12:37 -0400

    UPDATE: Conversion to production database

--- server/db.js
+++ server/db.js
@@ -1,12 +1,10 @@
 require('dotenv').config();
 const Pool = require('pg').Pool;
 
+ const connectionString = process.env.DB_CONNECTION;
+
 const pool = new Pool({
-    user: process.env.DB_USER,
-    password: process.env.DB_PASS,
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    database: process.env.DB_NAME
+    connectionString,
 });
 
 module.exports = pool;
